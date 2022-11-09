c299c56db26d49b3b21d3f6e0f6e13bccd5ee246
Md. Sazzad Siddique Likhon <mdsazzadsiddique0@gmail.com>
2022-11-09 15:04:21 +0600

    Initial commit

---
+++ LAB-6 [PHP] [DB CRUD Operations]/index.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>ADD PRODUCT</title>
+</head>
+<body>
+    <form action="">
+        <fieldset>
+            <legend>ADD PRODUCT</legend>
+            <label for="name">Name</label>
+            <br>
+            <input type="text" name="name" id="name">
+            <br>
+            <label for="buying_price">Buying Price</label>
+            <br>
+            <input type="number" name="buyingPrice" id="buying_price">
+            <br>
+            <label for="selling_price">Selling Price</label>
+            <br>
+            <input type="number" name="sellingPrice" id="selling_price">
+        </fieldset>
+    </form>
+</body>
+</html>

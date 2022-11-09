c87298361c54838393d3322a1127f898acd8657d
Md. Sazzad Siddique Likhon <mdsazzadsiddique0@gmail.com>
2022-11-09 15:52:47 +0600

    Fourth commit, 3: 52

--- LAB-6 [PHP] [DB CRUD Operations]/edit_product.php
+++ LAB-6 [PHP] [DB CRUD Operations]/edit_product.php
@@ -15,7 +15,7 @@
             <label for="name">Name</label>
             <br>
             <br>
-            <input type="text" name="name" id="name">
+            <input type="text" name="name" id="name" value="">
             <br>
             <br>
             <label for="buying_price">Buying Price</label>

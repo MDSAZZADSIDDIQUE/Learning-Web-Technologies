2f89c9f2f6f4fd91ba320ebc3f448437e2dd37b2
Md. Sazzad Siddique Likhon <mdsazzadsiddique0@gmail.com>
2022-11-09 15:14:15 +0600

    First commt, 3:14

--- LAB-6 [PHP] [DB CRUD Operations]/index.php
+++ LAB-6 [PHP] [DB CRUD Operations]/index.php
@@ -2,21 +2,44 @@
 <head>
     <title>ADD PRODUCT</title>
 </head>
+<style>
+    fieldset {
+        display: inline-block;
+        width: 50vw;
+    }
+</style>
 <body>
     <form action="">
         <fieldset>
-            <legend>ADD PRODUCT</legend>
+            <legend><h1>ADD PRODUCT</h1></legend>
             <label for="name">Name</label>
+            <br>
             <br>
             <input type="text" name="name" id="name">
             <br>
+            <br>
             <label for="buying_price">Buying Price</label>
+            <br>
             <br>
             <input type="number" name="buyingPrice" id="buying_price">
             <br>
+            <br>
             <label for="selling_price">Selling Price</label>
             <br>
+            <br>
             <input type="number" name="sellingPrice" id="selling_price">
+            <br>
+            <br>
+            <hr>
+            <br>
+            <input type="checkbox" name="display" id="display">
+            <label for="display">Display</label>
+            <br>
+            <br>
+            <hr>
+            <br>
+            <br>
+            <input type="submit" value="SAVE">
         </fieldset>
     </form>
 </body>

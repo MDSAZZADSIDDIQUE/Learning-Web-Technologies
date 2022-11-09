bb096288a18ef1e429da764d88277830a0f74ead
Md. Sazzad Siddique Likhon <mdsazzadsiddique0@gmail.com>
2022-11-09 15:51:56 +0600

    Third commit, 3:51

--- LAB-6 [PHP] [DB CRUD Operations]/display_products.php
+++ LAB-6 [PHP] [DB CRUD Operations]/display_products.php
@@ -5,13 +5,15 @@
 <style>
     fieldset {
         display: inline-block;
-        width: 50vw;
+    }
+    table, th, td {
+        border: 1px solid;
     }
 </style>
 <body>
     <form action="add_product.php" method="POST">
         <fieldset>
-            <legend>DISPLAY</legend>
+            <legend><h1>DISPLAY</h1></legend>
             <table>
                 <tr>
                     <th>NAME</th>
@@ -20,8 +22,16 @@
                 <?php
                 $connection = mysqli_connect('localhost', 'root', '', 'products_database');
                 $sql = "SELECT * FROM products_information";
-                $status = mysqli_query($connection, $sql);
-                while
+                $productsInformation = mysqli_query($connection, $sql);
+                while ($productInformation = mysqli_fetch_array($productsInformation)) {
+                    echo "<tr>";
+                    echo "<td>{$productInformation[0]}</td>";
+                    $profit = $productInformation[2] - $productInformation[1];
+                    echo "<td>{$profit}</td>";
+                    echo "<td><a href='edit_product.php'>edit</a></td>";
+                    echo "<td><a href=''>delete</a></td>";
+                    echo "</tr>";
+                }
                 ?>
             </table>
         </fieldset>

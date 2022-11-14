b6bc0b622949fe1b905ca6f3fdbfe515cd05ef69
Md. Sazzad Siddique Likhon <62556794+MDSAZZADSIDDIQUE@users.noreply.github.com>
2022-11-14 16:27:04 +0600

    Editing product edit

--- LAB-6 [PHP] [DB CRUD Operations]/add_product.php
+++ LAB-6 [PHP] [DB CRUD Operations]/add_product.php
@@ -3,8 +3,6 @@ $name = $_POST['name'];
 $buyingPrice = $_POST['buyingPrice'];
 $sellingPrice = $_POST['sellingPrice'];
 $display = $_POST['display'];
-
-
 if ($display == "display") {
     $connection = mysqli_connect('localhost', 'root', '', 'products_database');
     $sql = "INSERT INTO products_information VALUES('{$name}', '{$buyingPrice}', '{$sellingPrice}', 'Yes')";
@@ -14,5 +12,4 @@ if ($display == "display") {
     $sql = "INSERT INTO products_information VALUES('{$name}', '{$buyingPrice}', '{$sellingPrice}', 'No')";
     $status = mysqli_query($connection, $sql);
 }
-
 header('location: display_products.php');
